e7c40c70a0e6657f18322c4b5a795b4a515cf8ed
Steve Viselli <steve.viselli@gmail.com>
2015-02-21 00:51:37 +1100

    Implement starting and stopping the MusicBrainz server EC2 instance

---
+++ config/aws.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var AWSConfig = {
+  region: "us-east-1",
+  EC2: {
+    apiVersion: "2014-10-02"
+  },
+  musicBrainz: {
+    instanceId: "i-7b151e8a"
+  }
+};
+
+module.exports = AWSConfig;
